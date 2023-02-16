9ffd529ef04c3693f4de73a60f5ad0484ba985b7
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-02-16 10:46:40 -0400

    setup and first steps

---
+++ next.config.js
@@ -0,0 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
+
+const path = require('path')
+
+module.exports = {
+  webpack: config => {
+    config.resolve.alias['components'] = path.join(__dirname, 'components')
+    config.resolve.alias['public'] = path.join(__dirname, 'public')
+
+    return config
+  }
+}
